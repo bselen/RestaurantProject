0cb0f0bd856e2a9ff830a1eee805a61b22e4aac3
perez <perezj253@gator.uhd.edu>
2024-04-24 15:49:32 -0500

    Added some comments to Main.Java.

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ public class Main{
     public static void main(String[] args){
         IDandPasswords idandPasswords = new IDandPasswords();
 
-
+        //Load up the ManageUser class to be able to handle user info.
+        //Loads saved user info from the save file.
         ManageUser manageUser = new ManageUser("userList.dat");
         manageUser.loadUsers();
 
 
         //INSTANCE
+        //Open up the login page
         LoginPage loginPage = new LoginPage(manageUser);
 
 
