eef5d4c6935bf7caedcd951d2cc46b8539aa993a
dusdn_xmnslhp <Hughend1@gator.uhd.edu>
2024-04-24 20:57:47 -0500

    Wrote the function to write the menu to a txt file and then display menu to the monitor.

--- Menu.java
+++ Menu.java
@@ -16,81 +16,81 @@ import java.io.BufferedReader;
                 pzMenu.write("\n\t\t\t\t\tSmall(10in) \tMedium(14in) \tLarge(18in) \tX-Large(22in)\n");
                 pzMenu.write("CLASSICS:\n");
                 pzMenu.write("\nFive Cheese\t\t\t - \t");
-                pzMenu.write("   $7.99\t\t");
-                pzMenu.write("   $9.99\t\t");
-                pzMenu.write("   $12.99\t\t");
+                pzMenu.write("   $7.99\t");
+                pzMenu.write("   $9.99\t");
+                pzMenu.write("   $12.99\t");
                 pzMenu.write("   $16.99\n");
                 pzMenu.write("\n\tFresh mozzarella, provolone, asiago, muenster\n\t and parmesan cheese. Cheese lovers rejoice!\n");
 
                 pzMenu.write("\nMargarita\t\t\t - \t");
-                pzMenu.write("   $8.99\t\t");
-                pzMenu.write("   $10.99\t\t");
-                pzMenu.write("   $13.99\t\t");
+                pzMenu.write("   $8.99\t");
+                pzMenu.write("   $10.99\t");
+                pzMenu.write("   $13.99\t");
                 pzMenu.write("   $17.99\n");
                 pzMenu.write("\n\tFresh mozzarella, tomatoes, basil and tyme.\n\t       It's a classic for a reason.\n");
 
                 pzMenu.write("\nPepperoni\t\t\t - \t");
-                pzMenu.write("   $8.99\t\t");
-                pzMenu.write("   $11.99\t\t");
-                pzMenu.write("   $15.99\t\t");
+                pzMenu.write("   $8.99\t");
+                pzMenu.write("   $11.99\t");
+                pzMenu.write("   $15.99\t");
                 pzMenu.write("   $20.99\n");
-                pzMenu.write("\n\tCovered in fresh mozzarella and pepperoni.\n\t\t\t      Enough said.\n");
+                pzMenu.write("\n\tCovered in fresh mozzarella and pepperoni.\n\t\t      Enough said.\n");
 
                 pzMenu.write("\nSupreme\t\t\t\t - \t");
-                pzMenu.write("   $11.99\t\t");
-                pzMenu.write("   $15.99\t\t");
-                pzMenu.write("   $20.99\t\t");
+                pzMenu.write("   $11.99\t");
+                pzMenu.write("   $15.99\t");
+                pzMenu.write("   $20.99\t");
                 pzMenu.write("   $26.99\n");
-                pzMenu.write("\n\t Pepperoni, ham, beef, seasoned pork, Italian sausage,\n\tonions, mushrooms, green bell peppers and black olives.\n\t\t\t       Now that's a mouthful.\n");
+                pzMenu.write("\n\t Pepperoni, ham, beef, seasoned pork, Italian sausage,\n\tonions, mushrooms, green bell peppers and black olives.\n\t\t       Now that's a mouthful.\n");
 
                 pzMenu.write("\nMeat Lovers\t\t\t - \t");
-                pzMenu.write("   $11.99\t\t");
-                pzMenu.write("   $15.99\t\t");
-                pzMenu.write("   $20.99\t\t");
+                pzMenu.write("   $11.99\t");
+                pzMenu.write("   $15.99\t");
+                pzMenu.write("   $20.99\t");
                 pzMenu.write("   $26.99\n");
-                pzMenu.write("\n\t  Old-world pepperoni, Italian sausage, ham,\n\tapplewood smoked bacon, seasoned pork and beef.\n\t\t\t     Grandpa's favorite!\n");
+                pzMenu.write("\n\t  Old-world pepperoni, Italian sausage, ham,\n\tapplewood smoked bacon, seasoned pork and beef.\n\t\t     Grandpa's favorite!\n");
 
                 pzMenu.write("\nSPECIALTIES:\n");
                 pzMenu.write("\nBlanco Spinach\t\t\t - \t");
-                pzMenu.write("   $9.99\t\t");
-                pzMenu.write("   $12.99\t\t");
-                pzMenu.write("   $16.99\t\t");
+                pzMenu.write("   $9.99\t");
+                pzMenu.write("   $12.99\t");
+                pzMenu.write("   $16.99\t");
                 pzMenu.write("   $21.99\n");
-                pzMenu.write("\n\tCreamy Alfredo sauce, fresh spinach, feta, Parmesan-Asiago,\n\t\t\tprovolone and real fresh mozzarella cheese.\n");
+                pzMenu.write("\n\tCreamy Alfredo sauce, fresh spinach, feta, Parmesan-Asiago,\n\t\tprovolone and real fresh mozzarella cheese.\n");
 
                 pzMenu.write("\nChicken x Bacon Ranch\t\t - \t");
-                pzMenu.write("   $10.99\t\t");
-                pzMenu.write("   $13.99\t\t");
-                pzMenu.write("   $17.99\t\t");
+                pzMenu.write("   $10.99\t");
+                pzMenu.write("   $13.99\t");
+                pzMenu.write("   $17.99\t");
                 pzMenu.write("   $22.99\n");
-                pzMenu.write("\n\tHomestyle ranch, rotisserie grilled chicken \n\t    breast and applewood smoked bacon.\n\t\t   Is your mouth watering yet?\n");
+                pzMenu.write("\n\tHomestyle ranch, rotisserie grilled chicken \n\t    breast and applewood smoked bacon.\n\t\t Is your mouth watering yet?\n");
 
                 pzMenu.write("\nHawaiian\t\t\t - \t");
-                pzMenu.write("   $10.99\t\t");
-                pzMenu.write("   $13.99\t\t");
-                pzMenu.write("   $17.99\t\t");
+                pzMenu.write("   $10.99\t");
+                pzMenu.write("   $13.99\t");
+                pzMenu.write("   $17.99\t");
                 pzMenu.write("   $22.99\n");
-                pzMenu.write("\n\tClassic marinara sauce, slow-roasted ham, smoked \n\tbacon and tropical pineapple. Pineapple on pizza?\n\t\t\t     Let's fight about it!\n");
+                pzMenu.write("\n\tClassic marinara sauce, slow-roasted ham, smoked \n\tbacon and tropical pineapple. Pineapple on pizza?\n\t\t     Let's fight about it!\n");
 
                 pzMenu.write("\nBuffalo or Barbeque Chicken\t - \t");
-                pzMenu.write("   $9.99\t\t");
-                pzMenu.write("   $12.99\t\t");
-                pzMenu.write("   $16.99\t\t");
+                pzMenu.write("   $9.99\t");
+                pzMenu.write("   $12.99\t");
+                pzMenu.write("   $16.99\t");
                 pzMenu.write("   $21.99\n");
-                pzMenu.write("\n\tYour choice of tangy barbeque or spicy buffalo sauce, sharp\n\tcheddar cheese, grilled chicken and applewood smoked bacon.\n\t\t\t\t\t    Try it half/half!\n");
+                pzMenu.write("\n\tYour choice of tangy barbeque or spicy buffalo sauce, sharp\n\tcheddar cheese, grilled chicken and applewood smoked bacon.\n\t\t\t    Try it half/half!\n");
 
 
                 pzMenu.write("\nMeatball Marinara\t\t - \t");
-                pzMenu.write("   $9.99\t\t");
-                pzMenu.write("   $12.99\t\t");
-                pzMenu.write("   $16.99\t\t");
+                pzMenu.write("   $9.99\t");
+                pzMenu.write("   $12.99\t");
+                pzMenu.write("   $16.99\t");
                 pzMenu.write("   $21.99\n");
                 pzMenu.write("\n\t100% American beef meatballs and creamy ricotta\n\t    cheese on a bed of fresh mozzarella and\n\t       mouth watering homestyle marinara.\n");
 
                 pzMenu.write("\n\nCREATE YOUR OWN:\t\t - \t");
-                pzMenu.write("   $8.99\t\t");
-                pzMenu.write("   $11.99\t\t");
-                pzMenu.write("   $15.99\t\t");
+                pzMenu.write("   $8.99\t");
+                pzMenu.write("   $11.99\t");
+                pzMenu.write("   $15.99\t");
                 pzMenu.write("   $20.99\n");
                 pzMenu.write("\n\tChoose your favorite toppings and create your own masterpiece!\n");
 
