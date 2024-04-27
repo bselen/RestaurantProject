ac325522dd3694ffebe9b9b06f8f11e134045f4a
alex <abustos2468@gmail.com>
2024-04-27 11:29:33 -0500

    unconnected menu

--- src/Menu.java
+++ src/Menu.java
@@ -2,9 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-/* this menu is not connected at all to the main program and the buttons
- don't save or send any data */
+// this menu is not connected to the main program
+
 public class Menu implements ActionListener {
     JFrame frame = new JFrame("Menu");
     JPanel panel = new JPanel(new GridLayout(0, 2, 5, 5));
@@ -21,6 +23,10 @@ public class Menu implements ActionListener {
     JLabel mushroomPizza = new JLabel("Mushroom Pizza: $10");
     JButton mushroomPizzaButton = new JButton("Order");
     JButton submitButton = new JButton("Submit");
+    List<String> pizzasSelected = new ArrayList<>();
+    double total = 0;
+
+
 
     public Menu() {
         // Increase font size for JLabels
@@ -64,24 +70,47 @@ public class Menu implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == cheesePizzaButton) {
             System.out.println("cheesePizzaButton pressed");
+            pizzasSelected.add("Cheese Pizza - $7");
+            total += 7;
         }
         if(e.getSource() == pepperoniPizzaButton) {
             System.out.println("pepperoniPizzaButton pressed");
+            pizzasSelected.add("Pepperoni Pizza - $10");
+            total += 10;
         }
         if(e.getSource() == sausagePizzaButton) {
             System.out.println("sausagePizzaButton pressed");
+            pizzasSelected.add("Sausage Pizza - $10");
+            total += 10;
         }
         if(e.getSource() == baconPizzaButton) {
             System.out.println("baconPizzaButton pressed");
+            pizzasSelected.add("Bacon Pizza - $10");
+            total += 10;
         }
         if(e.getSource() == pineapplePizzaButton) {
             System.out.println("pineapplePizzaButton pressed");
+            pizzasSelected.add("Pineapple Pizza - $10");
+            total += 10;
         }
         if(e.getSource() == mushroomPizzaButton) {
             System.out.println("mushroomPizzaButton pressed");
+            pizzasSelected.add("Mushroom Pizza - $10");
+            total += 10;
         }
         if(e.getSource() == submitButton) {
             System.out.println("submitButton pressed");
+            System.out.println(pizzasSelected);
+            System.out.println(total);
+            frame.dispose();
         }
+
+    }
+    public List<String> getPizzasSelected() {
+        return pizzasSelected;
+    }
+
+    public double menuGetTotal() {
+        return total;
     }
 }
